88b90bd0e9c734d47ab7297c81b707ecf51b0881
Scarleth Mendoza <mendozamarcano91@gmail.com>
2026-01-07 17:43:34 -0300

    Ajuste

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -6,8 +6,7 @@ const app = express();
 
 const allowedOrigins = [
   "https://khalita.two-dd.com", 
-  "http://localhost:5173",     
-  "http://localhost:5174",     
+  "http://localhost:5173"       
 ];
 
 app.use(cors({
