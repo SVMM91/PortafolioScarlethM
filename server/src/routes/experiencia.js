07877a7675207610f03f1299e2902c871aa5f396
Scarleth Mendoza <mendozamarcano91@gmail.com>
2026-01-07 16:24:23 -0300

    Ajuste

--- server/src/routes/experiencia.js
+++ server/src/routes/experiencia.js
@@ -1,8 +1,11 @@
+
 import { Router } from 'express';
 import AllController from '../controllers/controllersAll.js'; 
 const router = Router();
 
 router.get('/', AllController.getAll);
-
+router.post('/', AllController.create);
+router.put('/:id', AllController.upDate);
+router.delete('/', AllController.deleteAll);
 
 export default router;
