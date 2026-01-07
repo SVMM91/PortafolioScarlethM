7dbbc1be4de18c16d81c590c03d531103459759d
Scarleth Mendoza <mendozamarcano91@gmail.com>
2026-01-07 16:07:04 -0300

    Ajuste

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -22,16 +22,13 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import experienciaRoutes from './routes/experiencia.js'; 
-
+import experienciaRoutes from '../routes/experiencia.js'; 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-
 app.use('/api', experienciaRoutes);
 
 const PORT = process.env.PORT || 3000;
