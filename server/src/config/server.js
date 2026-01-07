44cc2c04c2d9f1236c2f9b205a8546d085285b0b
Scarleth Mendoza <mendozamarcano91@gmail.com>
2026-01-07 16:00:48 -0300

    Ajuste

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -1,28 +1,6 @@
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get('/api/experiencia', (req, res) => {
-//   res.json({ mensaje: 'Experiencia OK' });
-// });
-
-// const PORT = process.env.PORT || 3000;
-// a
-// app.listen(PORT, () => {
-//   console.log(`Servidor corriendo en puerto ${PORT}`);
-// });
-
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import experienciaRoutes from './routes/experiencia.js'; // ⚠ .js obligatorio
 
 dotenv.config();
 
@@ -31,11 +9,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ✅ Usamos el router con AllController
-app.use('/api', experienciaRoutes);
+app.get('/api/experiencia', (req, res) => {
+  res.json({ mensaje: 'Experiencia OK' });
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
