e24bf054300aa9a811e842c9810ba38cb8ba2618
Scarleth Mendoza <mendozamarcano91@gmail.com>
2026-01-07 16:30:20 -0300

    Ajuste

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -1,34 +1,16 @@
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get('/api/experiencia', (req, res) => {
-//   res.json({ mensaje: 'Experiencia OK' });
-// });
-
-// const PORT = process.env.PORT || 3000;
-// a
-// app.listen(PORT, () => {
-//   console.log(`Servidor corriendo en puerto ${PORT}`);
-// });
-
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import experienciaRoutes from '../routes/experiencia.js'; 
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
 app.use('/api', experienciaRoutes);
 
 const PORT = process.env.PORT || 3000;
--- server/src/routes/experiencia.js
+++ server/src/routes/experiencia.js
@@ -1,9 +1,11 @@
-
 import { Router } from 'express';
-import AllController from '../controllers/controllersAll.js'; 
+import AllController from '../controllers/controllersAll.js';
+
 const router = Router();
 
-router.get('/', AllController.getAll);
+// Rutas
+router.get('/', AllController.getAll);        
+router.get('/:id', AllController.getOne);     
 router.post('/', AllController.create);
 router.put('/:id', AllController.upDate);
 router.delete('/', AllController.deleteAll);
