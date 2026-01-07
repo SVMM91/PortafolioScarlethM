1ad92f5c394a66a2374569cce11358c28ddffb30
Scarleth Mendoza <mendozamarcano91@gmail.com>
2026-01-07 16:02:03 -0300

    Ajuste

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -1,7 +1,28 @@
+// import express from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get('/api/experiencia', (req, res) => {
+//   res.json({ mensaje: 'Experiencia OK' });
+// });
+
+// const PORT = process.env.PORT || 3000;
+// a
+// app.listen(PORT, () => {
+//   console.log(`Servidor corriendo en puerto ${PORT}`);
+// });
+
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import experienciaRoutes from '../routes/experiencia'; 
 dotenv.config();
 
 const app = express();
@@ -9,13 +30,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/experiencia', (req, res) => {
-  res.json({ mensaje: 'Experiencia OK' });
-});
+app.use('/api', experienciaRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
-
