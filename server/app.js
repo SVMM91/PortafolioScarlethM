5a7a9191eecb6a052dc14ea6dde3fdaf79343316
Scarleth Mendoza <mendozamarcano91@gmail.com>
2026-01-07 16:27:06 -0300

    Ajuste

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import experienciaRoutes from './routes/experiencia.js';
+import experienciaRoutes from './src/routes/experiencia.js';
 
 const app = express();
 
