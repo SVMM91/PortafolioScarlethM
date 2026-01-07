1260c25de1997366bf05f8dee94dd5f9b15b3184
Scarleth Mendoza <mendozamarcano91@gmail.com>
2026-01-07 16:46:38 -0300

    Ajuste

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', experienciaRoutes);
+app.use('/api/experiencia', experienciaRoutes);
 
 app.get('/', (req, res) => {
   res.json({ ok: true, message: 'Backend activo' });
