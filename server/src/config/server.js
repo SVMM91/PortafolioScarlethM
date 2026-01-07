caca5a43c26fef322afcac1ca5611ef1ce11cdc8
Scarleth Mendoza <mendozamarcano91@gmail.com>
2026-01-07 17:04:18 -0300

    Ajuste

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -2,13 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import experienciaRoutes from '../routes/experiencia.js'; 
-
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: 'https://khalita.two-dd.com' 
+}));
+
 
 // router en /api/experiencia
 app.use('/api/experiencia', experienciaRoutes);
